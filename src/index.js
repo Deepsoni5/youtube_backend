099aa18168968b455d061f8065f24208ad972182
Deepsoni5 <fichadiyadeep@gmail.com>
2023-12-09 19:33:41 +0530

    connected to mongodb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-    
+//require("dotenv").config();
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({ path: "./env" });
+connectDB();
+
+// import express from "express";
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (err) => {
+//       console.log(err);
+//       throw err;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log("App listening on port " + process.env.PORT);
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// })();
